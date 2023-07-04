13566ab95b5331980f8297c385d4c98f582dace9
nicknicknick4444 <94404473+nicknicknick4444@users.noreply.github.com>
2023-07-04 22:50:13 +0100

    Add files via upload

--- src/components/hooks/prop-hooks.js
+++ src/components/hooks/prop-hooks.js
@@ -4,9 +4,10 @@ const PropContext = createContext();
 export const useProps = () => useContext(PropContext);
 
 export default function PropProvider({children}) {
-    const [winsignal, setWinsignal] = useState(false);
+    const [win, setWin] = useState(false);
+    // const [winsignal, setWinsignal] = useState(false);
 
-    return <PropContext.Provider value={{winsignal, setWinsignal}}>
+    return <PropContext.Provider value={{win, setWin}}>
         {children}
     </PropContext.Provider>
 };
